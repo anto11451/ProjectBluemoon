d9291c951075361353061d645900a7c797e61975
anto11451 <anto.anand111@gmail.com>
2025-04-06 23:26:51 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome to the Code Viewer</h1>
+      <h1>Welcome to the Wellbeing! </h1>
       <ul>
         <li><Link to="/working-code-alpha">How Healthy are you?</Link></li>
         <li><Link to="/activity-vs-calories">Activity vs Calories</Link></li>
